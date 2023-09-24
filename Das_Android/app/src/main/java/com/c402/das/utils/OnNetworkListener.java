527ae0400f34ff802bccafd094ca27119db0556c
ChidoriNC2 <122858863+dungnc2@users.noreply.github.com>
2023-09-24 22:03:20 +0700

    up

--- Das_Android/app/src/main/java/com/c402/das/utils/OnNetworkListener.java
+++
@@ -1,6 +0,0 @@
-package com.marwaeltayeb.das.utils;
-
-public interface OnNetworkListener {
-    void onNetworkConnected();
-    void onNetworkDisconnected();
-}
--- Das_Android/app/src/main/java/com/c402/das/utils/RequestCallback.java
+++
@@ -1,5 +0,0 @@
-package com.marwaeltayeb.das.utils;
-
-public interface RequestCallback {
-    void onCallBack();
-}
